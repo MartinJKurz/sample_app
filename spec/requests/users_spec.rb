9d564fc5926a5948849525a932373a5a38f117e6
Martin J. Kurz <martin.j.kurz@googlemail.com>
2011-07-12 01:50:42 +0200

    Done with user edit/update, index, and destroy actions

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -38,16 +38,22 @@ describe "Users" do
 
     describe "failure" do
       it "should not sign a user in" do
-
-		# what's wrong here?
-	    # us = {:email => "", :password => ""}
-	    # integration_sign_in( us )
 	  
-        visit signin_path
-        fill_in :email,    :with => ""
-        fill_in :password, :with => ""
-        click_button
-		
+		user = User.new(:email => "", :password => "")
+		integration_sign_in (user)
+	  
+        response.should have_selector("div.flash.error", :content => "Invalid")
+      end
+    end
+
+	#mkz
+    describe "failure" do
+      it "should not sign a user in 2" do
+	  
+        user = Factory(:user)
+		user.email = 'aha'
+		integration_sign_in (user)
+	  
         response.should have_selector("div.flash.error", :content => "Invalid")
       end
     end
@@ -55,10 +61,8 @@ describe "Users" do
     describe "success" do
       it "should sign a user in and out" do
         user = Factory(:user)
-        visit signin_path
-        fill_in :email,    :with => user.email
-        fill_in :password, :with => user.password
-        click_button
+		integration_sign_in (user)
+		
         controller.should be_signed_in
         click_link "Sign out"
         controller.should_not be_signed_in
